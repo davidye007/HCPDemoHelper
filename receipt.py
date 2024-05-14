7be90639faa8c60d6115b8b218bef73a2708774a
davidye007 <davidyeap@outlook.com>
2024-05-14 14:24:38 -0700

    all in one

--- receipt.py
+++ receipt.py
@@ -1,6 +1,54 @@
 from datetime import datetime
-# from store import Store
-# from item import Item
+
+class Store:
+
+    # dictionary of items objects
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+
+    @staticmethod
+    def from_dict(source):
+        name = source.get('name')
+        address = source.get('address')
+        return Store(name=name, address=address)
+
+    def to_dict(self):
+        return {
+        'name': self.name,
+        'address': self.address
+        }
+
+    def __repr__(self):
+        return f"store(name={self.name}, address={self.address})"
+
+class Item:
+
+    # TODO: add a field for amount (e.g. quantity/weight)
+
+    # dictionary of items objects
+    def __init__(self, name, price, amount):
+        self.name = name
+        self.price = price
+        self.amount = amount
+
+    # from_dict(source) is a static method
+    @staticmethod
+    def from_dict(source):
+        name = source.get('name')
+        price = source.get('price')
+        amount = source.get('amount')
+        return Item(name=name, price=price, amount=amount)
+
+    def to_dict(self):
+        return {
+        'name': self.name,
+        'price': self.price,
+        'amount': self.amount
+        }
+
+    def __repr__(self):
+        return f"item(name={self.name}, price={self.price}, amount={self.amount})"
 
 class Receipt:
 
